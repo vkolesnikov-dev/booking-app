c91e7ead94168e0fe9f187bc117e75fa89c1f1e0
Vitalik <kolesnikov.dev@yandex.ru>
2025-03-21 17:23:30 +0300

    fixed the code

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,8 +4,6 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 
 const AppHeader = () => {
   return (
@@ -19,20 +17,12 @@ const AppHeader = () => {
           >
             Meeting-scheduler
           </Typography>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ marginRight: 7, fontSize: 17 }}
-          >
+          <Button sx={{ marginRight: 10 }} color="inherit">
             Home
-          </Typography>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, fontSize: 17 }}
-          >
+          </Button>
+          <Button sx={{ marginRight: 130 }} color="inherit">
             Calendar
-          </Typography>
+          </Button>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
